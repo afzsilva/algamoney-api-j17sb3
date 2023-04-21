660b6bd8052a2edde2b22bc0308a345bdee88f83
afzsilva <afzsilva@gmail.com>
2023-04-21 15:13:40 -0300

    Evolui o HandlerException

--- src/main/java/br/com/afzdev/algamoneyapi/exceptionhandler/AlagamoneyExceptionHandler.java
+++ src/main/java/br/com/afzdev/algamoneyapi/exceptionhandler/AlagamoneyExceptionHandler.java
@@ -1,5 +1,8 @@
 package br.com.afzdev.algamoneyapi.exceptionhandler;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
@@ -12,8 +15,32 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @ControllerAdvice
 public class AlagamoneyExceptionHandler extends ResponseEntityExceptionHandler {
 
+    @Autowired
+    private MessageSource messageSource;
     @Override
     protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
-        return handleExceptionInternal(ex,"Request Inválido",headers, HttpStatus.BAD_REQUEST, request);
+        String mensagemUsuario = messageSource.getMessage("mensagem.invalida",null, LocaleContextHolder.getLocale());
+        String mensagemDesenvolvedor = ex.getCause().toString();
+        return handleExceptionInternal(ex,new Erro(mensagemUsuario,mensagemDesenvolvedor),headers, HttpStatus.BAD_REQUEST, request);
     }
+
+    public static class Erro{
+        private String mensagemUsuario;
+        private String mensagemDesenvolvedor;
+
+        public Erro(String mensagemUsuario, String mensagemDesenvolvedor) {
+            this.mensagemUsuario = mensagemUsuario;
+            this.mensagemDesenvolvedor = mensagemDesenvolvedor;
+        }
+
+        public String getMensagemUsuario() {
+            return mensagemUsuario;
+        }
+
+        public String getMensagemDesenvolvedor() {
+            return mensagemDesenvolvedor;
+        }
+    }
+
+
 }
