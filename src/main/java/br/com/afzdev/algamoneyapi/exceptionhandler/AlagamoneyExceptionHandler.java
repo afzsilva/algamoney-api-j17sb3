04f9df70a8a7cf3655e92de2c8c024d6a23e125a
afzsilva <afzsilva@gmail.com>
2023-04-21 14:45:40 -0300

    Inicia implementacao HandlerException

---
+++ src/main/java/br/com/afzdev/algamoneyapi/exceptionhandler/AlagamoneyExceptionHandler.java
@@ -0,0 +1,19 @@
+package br.com.afzdev.algamoneyapi.exceptionhandler;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class AlagamoneyExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        return handleExceptionInternal(ex,"Request Inválido",headers, HttpStatus.BAD_REQUEST, request);
+    }
+}
